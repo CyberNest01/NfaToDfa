9dc0755ef081f8f4de77338c7e765394d5af56de
hosein <b.mohammadi.m@gmail.com>
2023-10-18 02:38:55 +0330

    next: fix matrix and some bug fix and create def get_q0 move and next move

--- main.py
+++ main.py
@@ -5,19 +5,88 @@ from itertools import groupby
 class NfaToDfa:
 
     def __init__(self):
-        self.nfa_matrix = np.matrix(['main', 'end', 'value', 'is_end'])
-        self.dfa_matrix = np.matrix(['main', 'end', 'value', 'is_end'])
-        self.processes_count = 0
+        self.nfa_matrix = np.matrix(['main', 'end', 'value'])
+        self.dfa_matrix = np.matrix(['main', 'end', 'value'])
+        self.nfa_status_matrix = np.matrix(['main', 'is_end'])
+        self.dfa_status_matrix = np.matrix(['main', 'is_end'])
         self.directions = 0
-        self.loop_count = 0
+        self.nfa_count = 1
+        self.dfa_count = 1
+        self.nfa_status_count = 1
+        self.dfa_status_count = 1
 
     def main(self):
         self.input_nfa_process()
+        self.get_q0_move()
+        self.insert_none_dfa()
+        self.next_move()
         print(self.nfa_matrix)
+        print('\n')
+        print(self.dfa_matrix)
+        print('\n')
+        print(self.nfa_status_matrix)
+        print('\n')
+        print(self.dfa_status_matrix)
 
-    def change_nfa_to_dfa(self):
-        for i in range(1, self.loop_count):
-            pass
+    def next_move(self):
+        for i in self.dfa_matrix:
+            if i.item(0) == 'main' or i.item(1) == 'None':
+                continue
+            values = self.open_str(i.item(1))
+            value_0 = []
+            value_1 = []
+            is_end_main = []
+            for value in values:
+                find_q0 = np.where(self.nfa_status_matrix == value)
+                is_end = 'True' if self.nfa_status_matrix.item(int(find_q0[0][0]), 1) == 'True' else 'False'
+                for x in self.nfa_matrix:
+                    if x.item(0) == 'main':
+                        continue
+                    if x.item(0) == value:
+                        if x.item(2) == '0':
+                            value_0.append(x.item(1))
+                            is_end_main.append(is_end)
+                        else:
+                            value_1.append(x.item(1))
+                            is_end_main.append(is_end)
+            value_0 = [None] if len(value_0) == 0 else list(set(value_0))
+            value_1 = [None] if len(value_1) == 0 else list(set(value_1))
+            is_end_main = any(is_end_main)
+            value_0 = self.list_to_str(value_0)
+            value_1 = self.list_to_str(value_1)
+            self.insert_data('dfa', i.item(1), value_0, '0')
+            self.insert_data('dfa', i.item(1), value_1, '1')
+            self.insert_status('dfa', i.item(1), is_end_main)
+
+    def get_q0_move(self):
+        value_0 = []
+        value_1 = []
+        for i in self.nfa_matrix:
+            if i.item(0) == 'main':
+                continue
+            if i.item(0) == 'q0':
+                if i.item(2) == '0':
+                    value_0.append(i.item(1))
+                else:
+                    value_1.append(i.item(1))
+        value_0 = [None] if len(value_0) == 0 else value_0
+        value_1 = [None] if len(value_1) == 0 else value_1
+        find_q0 = np.where(self.nfa_status_matrix == 'q0')
+        is_end = 'True' if self.nfa_status_matrix.item(int(find_q0[1][0]), 1) == 'True' else 'False'
+        value_0 = self.list_to_str(value_0)
+        value_1 = self.list_to_str(value_1)
+        self.insert_data('dfa', 'q0', value_0, '0')
+        self.insert_data('dfa', 'q0', value_1, '1')
+        self.insert_status('dfa', 'q0', is_end)
+        self.insert_status('dfa', 'q0', is_end)
+        return value_0, value_1
+
+    def insert_none_dfa(self) -> None:
+        if len(np.where(self.dfa_matrix)[0]) > 0:
+            self.insert_data('dfa', 'None', 'None', '0')
+            self.insert_data('dfa', 'None', 'None', '1')
+            self.insert_status('dfa', 'None', 'False')
+            self.insert_status('dfa', 'None', 'False')
 
     def input_nfa_process(self) -> None:
         processes = int(input('How many Processes in your nfa: '))
@@ -28,9 +97,32 @@ class NfaToDfa:
             for direction in range(directions):
                 which_process = self.check_process(processes)
                 value = input('Put value: ')
-                self.loop_count += 1
-                self.nfa_matrix = np.insert(self.nfa_matrix, self.loop_count, (f'q{process}', which_process, value, is_end), axis=0)
-        self.processes_count = processes
+                self.insert_data('nfa', f'q{process}', which_process, value)
+            self.insert_status('nfa', f'q{process}', is_end)
+
+    def insert_data(self, nfa_or_dfa, process, which_process, value) -> None:
+        if nfa_or_dfa == 'nfa':
+            self.nfa_matrix = np.insert(self.nfa_matrix, self.nfa_count, (process, which_process, value), axis=0)
+            self.nfa_count += 1
+        else:
+            self.dfa_matrix = np.insert(self.dfa_matrix, self.dfa_count, (process, which_process, value), axis=0)
+            self.dfa_count += 1
+
+    def insert_status(self, nfa_or_dfa, process, is_end):
+        if nfa_or_dfa == 'nfa':
+            self.nfa_status_matrix = np.insert(self.nfa_status_matrix, self.nfa_status_count, (process, is_end), axis=0)
+            self.nfa_status_count += 1
+        else:
+            self.dfa_status_matrix = np.insert(self.dfa_status_matrix, self.dfa_status_count, (process, is_end), axis=0)
+            self.dfa_status_count += 1
+
+    @staticmethod
+    def list_to_str(value):
+        return f'{",".join(str(e) for e in value)}'
+
+    @staticmethod
+    def open_str(value) -> list:
+        return value.split(",")
 
     @staticmethod
     def check_process(count_processes: int) -> str:
@@ -47,4 +139,3 @@ class NfaToDfa:
 
 if __name__ == '__main__':
     NfaToDfa().main()
-
