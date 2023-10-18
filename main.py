96c1697428c2430c2aedb9a7bafa576e86053484
hosein <b.mohammadi.m@gmail.com>
2023-10-19 01:27:57 +0330

    next: next level project

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ from itertools import groupby
 class NfaToDfa:
 
     def __init__(self):
-        self.nfa_matrix = np.matrix(['main', 'end', 'value'])
-        self.dfa_matrix = np.matrix(['main', 'end', 'value'])
-        self.nfa_status_matrix = np.matrix(['main', 'is_end'])
-        self.dfa_status_matrix = np.matrix(['main', 'is_end'])
-        self.directions = 0
+        self.nfa_matrix = np.matrix(['_____main_____', '_____end_____', '_____value_____'])
+        self.dfa_matrix = np.matrix(['_____main_____', '_____end_____', '_____value_____'])
+        self.nfa_status_matrix = np.matrix(['_____main_____', '_____is_end_____'])
+        self.dfa_status_matrix = np.matrix(['_____main_____', '_____is_end_____'])
         self.nfa_count = 1
         self.dfa_count = 1
         self.nfa_status_count = 1
@@ -18,8 +17,9 @@ class NfaToDfa:
     def main(self):
         self.input_nfa_process()
         self.get_q0_move()
-        self.insert_none_dfa()
+        # self.insert_none_dfa()
         self.next_move()
+        self.check_end_dfa()
         print(self.nfa_matrix)
         print('\n')
         print(self.dfa_matrix)
@@ -28,9 +28,16 @@ class NfaToDfa:
         print('\n')
         print(self.dfa_status_matrix)
 
-    def next_move(self):
+    def check_end_dfa(self) -> None:
         for i in self.dfa_matrix:
-            if i.item(0) == 'main' or i.item(1) == 'None':
+            if i.item(0) == '_____main_____' or i.item(1) == 'None':
+                continue
+            if 0 not in np.where(self.dfa_matrix == i.item(1))[1]:
+                self.next_move()
+
+    def next_move(self) -> None:
+        for i in self.dfa_matrix:
+            if i.item(0) == '_____main_____':
                 continue
             values = self.open_str(i.item(1))
             value_0 = []
@@ -38,31 +45,35 @@ class NfaToDfa:
             is_end_main = []
             for value in values:
                 find_q0 = np.where(self.nfa_status_matrix == value)
-                is_end = 'True' if self.nfa_status_matrix.item(int(find_q0[0][0]), 1) == 'True' else 'False'
+                is_end = 1 if self.nfa_status_matrix.item(int(find_q0[0][0]), 1) == 'True' else 0
+                is_end_main.append(is_end)
+
                 for x in self.nfa_matrix:
-                    if x.item(0) == 'main':
+                    if x.item(0) == '_____main_____':
                         continue
                     if x.item(0) == value:
                         if x.item(2) == '0':
                             value_0.append(x.item(1))
-                            is_end_main.append(is_end)
                         else:
                             value_1.append(x.item(1))
-                            is_end_main.append(is_end)
+                if value == i.item(0):
+                    break
             value_0 = [None] if len(value_0) == 0 else list(set(value_0))
             value_1 = [None] if len(value_1) == 0 else list(set(value_1))
+            print(value_0)
             is_end_main = any(is_end_main)
             value_0 = self.list_to_str(value_0)
             value_1 = self.list_to_str(value_1)
-            self.insert_data('dfa', i.item(1), value_0, '0')
-            self.insert_data('dfa', i.item(1), value_1, '1')
-            self.insert_status('dfa', i.item(1), is_end_main)
+            if 0 not in np.where(self.dfa_matrix == i.item(1))[1]:
+                self.insert_data('dfa', i.item(1), value_0, '0')
+                self.insert_data('dfa', i.item(1), value_1, '1')
+                self.insert_status('dfa', i.item(1), is_end_main)
 
     def get_q0_move(self):
         value_0 = []
         value_1 = []
         for i in self.nfa_matrix:
-            if i.item(0) == 'main':
+            if i.item(0) == '_____main_____':
                 continue
             if i.item(0) == 'q0':
                 if i.item(2) == '0':
@@ -72,12 +83,11 @@ class NfaToDfa:
         value_0 = [None] if len(value_0) == 0 else value_0
         value_1 = [None] if len(value_1) == 0 else value_1
         find_q0 = np.where(self.nfa_status_matrix == 'q0')
-        is_end = 'True' if self.nfa_status_matrix.item(int(find_q0[1][0]), 1) == 'True' else 'False'
+        is_end = 'True' if self.nfa_status_matrix.item(int(find_q0[0][0]), 1) == 'True' else 'False'
         value_0 = self.list_to_str(value_0)
         value_1 = self.list_to_str(value_1)
         self.insert_data('dfa', 'q0', value_0, '0')
         self.insert_data('dfa', 'q0', value_1, '1')
-        self.insert_status('dfa', 'q0', is_end)
         self.insert_status('dfa', 'q0', is_end)
         return value_0, value_1
 
@@ -93,7 +103,6 @@ class NfaToDfa:
         for process in range(processes):
             is_end = bool(int(input(f'Is q{process} final processing? (if yes put 1 if no put 0)')))
             directions = int(input(f'How many Direction in your nfa q{process}: '))
-            self.directions += directions
             for direction in range(directions):
                 which_process = self.check_process(processes)
                 value = input('Put value: ')
@@ -118,6 +127,7 @@ class NfaToDfa:
 
     @staticmethod
     def list_to_str(value):
+        value.sort()
         return f'{",".join(str(e) for e in value)}'
 
     @staticmethod
